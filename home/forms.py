57f4aa96621aa9cadab19d87185219af768f9443
Charlie Medina <charliegmv@hotmail.com>
2016-01-21 19:25:29 -0500

    Login UI

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,7 @@
+# -*- encoding: utf-8 -*-
 from django import forms
 #from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput())
-    password = forms.CharField(widget=forms.PasswordInput(render_value=False))
+    username = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'Usuario'}))
+    password = forms.CharField(label="",widget=forms.PasswordInput(render_value=False, attrs={'placeholder': 'Contraseña'}))
