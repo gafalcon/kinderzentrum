44505439026843df2d5c214998b6a0f3c1fd2fc2
Ruben Carvajal <ruben.carvajal@cti.espol.edu.ec>
2016-01-21 19:28:28 -0500

    Agregando logout

--- home/forms.py
+++ home/forms.py
@@ -4,3 +4,16 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput())
     password = forms.CharField(widget=forms.PasswordInput(render_value=False))
+
+
+class FichaForm(forms.Form):
+	SEXO=[('masculino','Masculino'),('femenino','Femenino')]
+    GRUPO_SANGUINEO = [('o+','O+'),('o-','O-')]
+    apellidos = forms.CharField(widget=forms.TextInput())
+    nombres = forms.CharField(widget=forms.TextInput())
+    nacimiento = forms.DateField(initial=datetime.date.today)
+    sexo = forms.ChoiceField(choices=SEXO, widget=forms.RadioSelect())
+    nacionalidad = forms.CharField(widget=forms.TextInput())
+    lugar_nacimiento = forms.CharField(widget=forms.TextInput())
+    grupo_sanguineo = forms.CharField(widget=forms.TextInput())
+    #password = forms.CharField(widget=forms.PasswordInput(render_value=False))
--- home/views.py
+++ home/views.py
@@ -5,8 +5,20 @@ from django.template import RequestContext
 from home.forms import LoginForm
 
 # Create your views here.
+
+
 def index_view(request):
-    return render_to_response('base.html', context_instance=RequestContext(request))
+    if request.user.is_authenticated():
+        grupos = request.user.groups.all()
+        '''
+        Aqui vamos a colocar las condiciones para que se habiliten las condiciones que el usuario
+        va a usar
+        '''
+        registro = grupos.filter(name='registro').count() == 1
+        ctx = {'registro':registro}
+        return render_to_response('base.html', ctx, context_instance=RequestContext(request))
+    else:
+        return render_to_response('base.html', context_instance=RequestContext(request))
 
 def formulario_view(request):
 	return render_to_response('registro/formulario.html', context_instance=RequestContext(request))
@@ -15,7 +27,7 @@ def login_view(request):
     mensaje = ""
     if request.user.is_authenticated():
     	#si esta autenticado redirecciona al formulario
-        return HttpResponseRedirect('/formulario')
+        return HttpResponseRedirect('/')
     else:
         if request.method == "POST":
             form = LoginForm(request.POST)
@@ -25,7 +37,7 @@ def login_view(request):
                 usuario = authenticate(username=username,password=password)
                 if usuario is not None and usuario.is_active:
                     login(request,usuario)
-                    return HttpResponseRedirect('/formulario')
+                    return HttpResponseRedirect('/')
                 else:
                     mensaje = "Usuario y/o password incorrecto"
         form = LoginForm()
