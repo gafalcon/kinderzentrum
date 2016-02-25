ba2951cc63183df44cbbb2c415f8e1ce591b311b
Charlie Medina <charliegmv@hotmail.com>
2016-02-24 22:50:23 -0500

    Mejora interfaz v1

--- home/forms.py
+++ home/forms.py
@@ -12,37 +12,37 @@ class LoginForm(forms.Form):
 class Ficha_DatosForm(forms.Form):
     SEXO=[('masculino','Masculino'),('femenino','Femenino')]
     GRUPO_SANGUINEO = [('o+','O+'),('o-','O-'),('a+','A+'),('a-','A-'),('b+','B+'),('b-','B-'),('ab+','AB+'),('ab-','AB-')]
-    apellidos = forms.CharField(widget=forms.TextInput())
-    nombres = forms.CharField(widget=forms.TextInput())
+    apellidos = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    nombres = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     nacimiento = forms.DateField(widget=forms.TextInput(attrs=
                                 {
-                                    'class':'datepicker'
+                                    'class':'datepicker form-control'
                                 }))
-    sexo = forms.ChoiceField(choices=SEXO, widget=forms.RadioSelect())
-    nacionalidad = forms.CharField(widget=forms.TextInput())
-    lugar_nacimiento = forms.CharField(widget=forms.TextInput())
-    grupo_sanguineo = forms.ChoiceField(choices=GRUPO_SANGUINEO)
+    sexo = forms.ChoiceField(choices=SEXO, widget=forms.RadioSelect())    
+    nacionalidad = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    lugar_nacimiento = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    grupo_sanguineo = forms.ChoiceField(choices=GRUPO_SANGUINEO, widget=forms.Select(attrs={'class':'form-control'}))
 
 class Ficha_DatosFamiliaresForm(forms.Form):
     ESTUDIO=[('primaria','Primaria'),('secundaria','Secundaria'),('universitaria','Universitaria'),('superior','Superior')]
-    apellidos = forms.CharField(widget=forms.TextInput())
-    nombres = forms.CharField(widget=forms.TextInput())
-    parentesco = forms.CharField(widget=forms.TextInput())
-    nivel_estudio = forms.ChoiceField(choices=ESTUDIO)
+    apellidos = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    nombres = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    parentesco = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    nivel_estudio = forms.ChoiceField(choices=ESTUDIO, widget=forms.Select(attrs={'class':'form-control'}))
     #trabajo
-    empresa = forms.CharField(widget=forms.TextInput())
-    direccion = forms.CharField(widget=forms.TextInput())
-    empresa = forms.CharField(widget=forms.TextInput())
+    empresa = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    direccion = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    empresa = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     #domicilio
-    direccion_domicilio = forms.CharField(widget=forms.TextInput())
-    telefono = forms.CharField(widget=forms.TextInput())
+    direccion_domicilio = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    telefono = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 
 class Ficha_DatosMedicoForm(forms.Form):
-    apellidos = forms.CharField(widget=forms.TextInput())
-    nombres = forms.CharField(widget=forms.TextInput())
-    area = forms.CharField(widget=forms.TextInput())
-    direccion = forms.CharField(widget=forms.TextInput())
-    telefono = forms.CharField(widget=forms.TextInput())
+    apellidos = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    nombres = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    area = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    direccion = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    telefono = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
         
 
         
---
+++ static/js/registro.js
@@ -0,0 +1,54 @@
+var page=1, total_pages=3
+
+function showButton(id,btnClass){
+	var obj = $("#"+id);
+	obj.attr("class","show "+btnClass);
+}
+
+function hideButton(id,btnClass){
+	var obj = $("#"+id)
+	obj.attr("class","hide "+btnClass)
+}
+
+function showPanel(id){
+	var obj = $("#"+id)
+	obj.attr("class","show panel panel-default")
+}
+
+function hidePanel(id){
+	var obj = $("#"+id)
+	obj.attr("class","hide panel panel-default")
+}
+
+function setProgressBar(prog){
+	var bar = $("#progress-bar")
+	bar.html(prog+" %")
+	bar.attr("style","width: "+prog+"%")	
+}
+
+function nextPanel(){
+	hidePanel("panel"+page)
+	page++
+	showPanel("panel"+page)
+	showButton("btn_previous","previous")
+	setProgressBar(Math.round((page-1)*100/total_pages))
+	if(page==total_pages){
+		hideButton("btn_next","next")
+	}	
+	
+}
+
+function previousPanel(){
+	hidePanel("panel"+page)
+	page--
+	showPanel("panel"+page)
+	showButton("btn_next","next")
+	setProgressBar(Math.round((page-1)*100/total_pages))
+	if(page==1){
+		hideButton("btn_previous","previous")
+		setProgressBar()
+		setProgressBar(0)
+	}
+	
+}
+
