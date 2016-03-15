05151be03a1890f2f93ef7e125401e5a6aa4222c
Ruben Carvajal <ruben.carvajal@cti.espol.edu.ec>
2016-03-15 00:20:07 -0500

    agregando descripción del niño en formulario + javascript para manejo de opciones dinámicas

--- registro/forms.py
+++ registro/forms.py
@@ -10,7 +10,7 @@ import datetime
 
 
 
-class Ficha_DatosForm(forms.Form):
+class Ficha_PacienteForm(forms.Form):
     SEXO=[('masculino','Masculino'),('femenino','Femenino')]
     GRUPO_SANGUINEO = [('o+','O+'),('o-','O-'),('a+','A+'),('a-','A-'),('b+','B+'),('b-','B-'),('ab+','AB+'),('ab-','AB-')]
     apellidos = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
@@ -44,6 +44,34 @@ class Ficha_DatosMedicoForm(forms.Form):
     area = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     direccion = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     telefono = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+   
+
+class Ficha_DescripcionPacienteForm(forms.Form):
+    CHOICES = [('papa','Papa'),('mama','Mama'),('abuelos','Abuelos'),('otros','Otros')]
+    CHOICES_SI_NO = [('si','Si'),('no','No')]
+    CHOICES_TERAPIA = [('rehabilitacion_fisica','Rehabilitacion Fisica'),('estimulacion_temprana','Estimulacion Temprana'),('ninguna','Ninguna')]
+    CHOICES_DIFICULTADES = [('audicion','Audicion'),('vision','Vision'),('lenguaje','Lenguaje'),('seguridad_si_mismo','Seguridad en si mismo'),('comportamiento','Comportamiento'),('alimentacion','Alimentacion'),('suenio','Suenio'),('interaccion_social','Interacion social'),('otro','Otro')]
+    CHOICES_SI_NO_DES = [('desconoce','Desconoce'),('si','Si'),('no','No')]
+    descripcion_pregunta_1 = forms.CharField(widget=forms.Textarea(attrs={'rows':'3'}), label="Que le preocupa de su hijo(a), algo especial que le llame la atencion?")
+    descripcion_pregunta_2 = forms.ChoiceField(choices=CHOICES, widget=forms.Select, label="Quien descubrio estas molestias?")
+    descripcion_otros_2 = forms.CharField(widget=forms.TextInput(attrs={'oculto':'oculto'}), initial='Especifique')
+    descripcion_pregunta_3 = forms.ChoiceField(choices=CHOICES_SI_NO, widget=forms.RadioSelect, label="Se encuentra en algun tratamiento?")
+    descripcion_pregunta_4 = forms.CharField(widget=forms.Textarea(attrs={'rows':'3','oculto':'oculto'}), label="En donde realiza el tratamiento?")
+    descripcion_pregunta_5 = forms.MultipleChoiceField(required=True,choices=CHOICES_TERAPIA, widget=forms.CheckboxSelectMultiple, label="Que tipo de terapia realiza?")
+    descripcion_tiempo_rehab_fisica =  forms.CharField(label="Tiempo en terapia de rehabilitacion fisica?", widget=forms.TextInput(attrs={'oculto':'oculto'}), initial='Especifique tiempo')
+    descripcion_tiempo_estimu_temprana = forms.CharField(label="Tiempo en terapia de estimulacion temprana?", widget=forms.TextInput(attrs={'oculto':'oculto'}), initial='Especifique tiempo')
+    descripcion_pregunta_6 = forms.MultipleChoiceField(required=True,choices=CHOICES_DIFICULTADES, widget=forms.CheckboxSelectMultiple, label="Ha presentado su hijo(a) algun tipo de dificultades en estas aereas? marque todas las opciones que desee.")
+    descripcion_otros_6 = forms.CharField(widget=forms.TextInput(attrs={'oculto':'oculto'}), initial='Especifique',label="Especifique otros")
+    descripcion_pregunta_7 = forms.ChoiceField(choices=CHOICES_SI_NO_DES, widget=forms.Select, label="Existe alguna limitacion con sus movimientos?")
+    descripcion_pregunta_8 = forms.ChoiceField(choices=CHOICES_SI_NO_DES, widget=forms.Select, label="Ha tenido convulsiones?")
+    descripcion_pregunta_8_1 = forms.CharField(widget=forms.Textarea(attrs={'rows':'2','oculto':'oculto'}), label="Que tipo de crisis tuvo durante la convulsion?")
+    descripcion_pregunta_8_2 = forms.CharField(widget=forms.Textarea(attrs={'rows':'1','oculto':'oculto'}), label="A que edad fue la primera crisis?")
+    descripcion_pregunta_8_3 = forms.ChoiceField(choices=CHOICES_SI_NO, widget=forms.Select(attrs={'oculto':'oculto'}), label="Tomo medicamentos?")
+    descripcion_pregunta_8_4 = forms.CharField(widget=forms.Textarea(attrs={'rows':'2','oculto':'oculto'}), label="Que medicamentos y dosis diaria tomo?")
+    
+
+class Ficha_HistorialMadreForm(forms.Form):
+    
         
 
 class HistorialMadreForm(ModelForm):
@@ -51,6 +79,9 @@ class HistorialMadreForm(ModelForm):
         model = HistorialMadre
         fields = ['perdidas_gestacionales', 'hijos_muertos', 'num_hijos_nacidos_muertos',
                   'num_hijos_nacidos_vivos', 'enfermedades_previas', 'anticonceptivos']
+
+
+
 
 class PacienteForm(ModelForm):
     class Meta:
--- registro/views.py
+++ registro/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, render_to_response
-from registro.forms import *#Ficha_DatosForm, Ficha_DatosFamiliaresForm, Ficha_DatosMedicoForm, HistorialMadreForm
+from registro.forms import * #Ficha_DatosForm, Ficha_DatosFamiliaresForm, Ficha_DatosMedicoForm, HistorialMadreForm
 from django.template import RequestContext
 
 # Create your views here.
@@ -10,20 +10,22 @@ def registro_view(request):
     else:
         mensaje = "enviando forma"
     #datos 
-    datos = Ficha_DatosForm()
+    datos = Ficha_PacienteForm()
     datos_familia = Ficha_DatosFamiliaresForm()
     datos_medico = Ficha_DatosMedicoForm()
     historial_madre = HistorialMadreForm()
     paciente = PacienteForm()
     madre = MadreForm()
     padre = PadreForm()
+    descripcion_paciente = Ficha_DescripcionPacienteForm()
     ctx = {'ficha_datos_medico_form':datos_medico,
            'ficha_datos_familia_form':datos_familia,
            'ficha_datos_form':datos,
+           'descripcion_paciente':descripcion_paciente,
            'historial_madre_form': historial_madre,
            'paciente': paciente,
            'padre': padre,
            'madre': madre,
-           'mensaje':mensaje
+           'mensaje':mensaje,
            'pagina_actual':'registro'}
     return render_to_response('registro/registro_ficha_medica.html',ctx,context_instance=RequestContext(request))
