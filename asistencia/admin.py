de053136e309d1054a99263b545678f0e2c06dcb
= <kevin_r_s_ch@hotmail.com>
2016-04-07 13:47:03 -0500

    base de datos restructurada

--- asistencia/admin.py
+++ asistencia/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from asistencia.models import Bebe,Terapia,Cit
+from asistencia.models import Bebe,Terapia,Cit,Tipo_terapia,Terapista,Terapia__Tipo_terapia
 
 admin.site.register(Bebe)
 admin.site.register(Terapia)
 admin.site.register(Cit)
+
+admin.site.register(Tipo_terapia)
+admin.site.register(Terapista)
+admin.site.register(Terapia__Tipo_terapia)
--- cita/modelos/cita_model.py
+++ cita/modelos/cita_model.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 #from __future__ import unicode_literals
 from django.db import models
-from registro.modelos.descripcion_models import* #import Terapia, Medicamento, Terapista
-#from registro.modelos.terapista_model import *
+
 from registro.modelos.paciente_model import Paciente
+from asistencia.modelos.Tipo_terapia_model import Tipo_terapia
 
 
 class Cita(models.Model):
+
+    ESTADO_CHOICES = (
+        ("A", "Agendada"),
+        ("S", "Asistio"),
+        ("N", "No asistio"),
+        ("C", "Cancelada")
+    )
     """ Representa la cita del paciente """
-    fecha_registro = models.DateTimeField( auto_now = True )
-    fecha_cita = models.DateTimeField( auto_now = True )
+    fecha_registro = models.DateField(auto_now =True)
+    fecha_cita = models.DateField(auto_now=False)
     hora_inicio = models.TimeField(auto_now=False)
     hora_fin = models.TimeField(auto_now=False)
-    cancelo_cita = models.BooleanField()
-    asistio_cita = models.CharField(max_length=1)
+    estado = models.CharField(choices=ESTADO_CHOICES, max_length=2, default='A')
     indicaciones = models.CharField(max_length=256)  
-    terapista_id = models.ForeignKey(Terapista, on_delete=models.CASCADE)
-    paciente_id = models.ForeignKey(Paciente, on_delete=models.CASCADE)    
+    tipo_terapia = models.ForeignKey(Tipo_terapia, on_delete=models.CASCADE)
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
 
 
     def __unicode__(self):
