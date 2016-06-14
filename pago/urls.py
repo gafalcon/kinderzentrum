dd2e3fd7bedbc177fdd5a6a6ef09647e69787190
gafalcon <gafalcon@espol.edu.ec>
2016-06-14 10:16:43 -0500

    url pago

--- pago/urls.py
+++ pago/urls.py
@@ -3,7 +3,7 @@ from pago import views
 
 urlpatterns = [
     # Examples:
-  url(r'^$', views.index_view, name='index_view'),
+  url(r'^$', views.index_view, name='pago_index_view'),
   url(r'^pacientes/sugerencias/$', views.patient_suggestions),
   url(r'^pacientes/(?P<patientId>\w{0,50})/$', views.patient_payments),
   # url(r'^pacientes/pdf/$', views.to_pdf),
