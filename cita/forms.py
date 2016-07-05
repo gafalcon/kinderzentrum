331d759101b0edb97679f766d665227d8ad12ca7
Raul Rodriguez <rsrodrig@espol.edu.ec>
2016-07-04 20:59:18 -0500

    Editar cita, eliminar cita

--- cita/forms.py
+++ cita/forms.py
@@ -1,7 +1,62 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+import datetime
 from django import forms
 from django.forms import ModelForm
 from django.forms.extras.widgets import SelectDateWidget
-#from django.core.exceptions import ValidationError
-#import datetime
+from django.forms import inlineformset_factory, BaseInlineFormSet, formset_factory, modelformset_factory
+from modelos.cita_model import Cita
 
+
+HORARIO_CHOICES = (
+        ("08:00", "08:00"),
+        ("08:30", "08:30"),
+        ("09:00", "09:00"),
+        ("09:30", "09:30"),
+        ("10:00", "10:00"),
+        ("10:30", "10:30"),
+        ("11:00", "11:00"), 
+        ("11:30", "11:30"),   
+        ("12:00", "12:00"),
+        ("12:30", "12:30"),
+        ("13:00", "13:00"),
+        ("13:30", "13:30"),
+        ("14:00", "14:00"),
+        ("14:30", "14:30"),
+        ("15:00", "15:00"), 
+        ("15:30", "15:30"),   
+        ("16:00", "16:00"),
+        ("16:30", "16:30"),
+        ("17:00", "17:00"),
+        ("17:30", "17:30"),
+        ("18:00", "18:00"),
+        ("18:30", "18:30"),
+        ("19:00", "19:00")            
+    )
+
+DATEFORMAT = '%d/%m/%Y'
+
+class CitaForm(ModelForm):
+    # grupo_sanguineo = forms.ChoiceField(choices=Paciente.GRUPO_SANGUINEO_CHOICES,
+    #                                     widget=forms.Select(attrs={'class':'form-control', 'required': 'required'}))
+    error_css_class = 'has-error'
+    fecha_cita = forms.DateField(input_formats=[DATEFORMAT],
+                                       label='Fecha de cita',
+                                       widget=forms.DateInput(attrs=
+                                                              {
+                                                                  'class':'datepicker form-control',
+                                                                  'required': 'required'
+                                                              },
+                                                              format=DATEFORMAT))
+    class Meta:
+        model = Cita
+        fields = ['fecha_cita',
+                  'hora_inicio',
+                  'hora_fin',
+                  'estado',
+                  'indicaciones',
+                  'tipo_terapia',
+                  'paciente',
+                  'terapista']
+        widgets = {'hora_inicio': forms.Select(choices=HORARIO_CHOICES,  attrs={'class':'form-control', 'required': 'required'}),
+                   'hora_fin': forms.Select(choices=HORARIO_CHOICES,  attrs={'class':'form-control', 'required': 'required'})
+        }
--- cita/views.py
+++ cita/views.py
@@ -1,144 +1,124 @@
-from django.contrib.auth.decorators                     import login_required
-from django.forms                                       import formset_factory
-from django.http                                        import HttpResponseRedirect
-from django.shortcuts                                   import render, render_to_response
-from django.template                                    import RequestContext
-from django.utils.decorators                            import method_decorator
-from django.views.generic                               import View
-from modelos.cita_model                                 import Cita
-from asistencia.modelos.Terapista_model                 import Terapista
-from asistencia.modelos.Tipo_terapia_model              import Tipo_terapia
-from asistencia.modelos.Terapia__Tipo_terapia_models    import Terapia__Tipo_terapia
-from registro.modelos.paciente_model                    import Paciente
+# -*- coding: utf-8 -*-
+from django.shortcuts import render, render_to_response, redirect, get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.views.generic import View, ListView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.forms import formset_factory
+from django.db.models import Q
+from django.core import serializers
+from django.http.response import HttpResponse
+import json
+from cita.forms import *
+from modelos.cita_model  import Cita
+from asistencia.modelos.Terapista_model  import Terapista
+from asistencia.modelos.Tipo_terapia_model  import Tipo_terapia
+from asistencia.modelos.Terapia__Tipo_terapia_models  import Terapia__Tipo_terapia
+from registro.modelos.paciente_model  import Paciente
 
 
 @method_decorator(login_required, name="dispatch")
 class ReservarCitaView(View):
     template_name = 'cita/reservar_cita.html'
-
-    template_name = 'cita/reservar_cita.html'
     def get(self, request, *args, **kwargs):
-        pacientes = Paciente.objects.order_by('fecha_registro')
-        tipo_terapia_list = Tipo_terapia.objects.order_by('costo')
-        terapista_list = Terapista.objects.order_by('fecha_nacimiento')
-        return render(request,self.template_name,{'pacientes':pacientes, 'tipo_terapia_list':tipo_terapia_list, 'terapista_list':terapista_list, 'pagina_actual': 'cita'})
+        datosCita = CitaForm(prefix="cita")
+        return render(request,self.template_name,{'datosCitaForm':datosCita, 'pagina_actual': 'cita'})
 
     def post(self, request, *args, **kwargs):
-        fechacita = request.POST.get('date_holder', default = "")
-        horaini = request.POST.get('horainicio', default = "")
-
-        if horaini == "08:00":
-            horaini = "08:00:00"        
-        elif horaini == "08:30":
-            horaini = "08:30:00"        
-        elif  horaini == "09:00":
-            horaini = "09:00:00"
-        if horaini == "09:30":
-            horaini = "09:30:00"        
-        elif horaini == "10:00":
-            horaini = "10:00:00"        
-        elif  horaini == "10:30":
-            horaini = "10:30:00"
-        if horaini == "11:00":
-            horaini = "11:00:00"        
-        elif horaini == "11:30":
-            horaini = "11:30:00"        
-        elif  horaini == "12:00":
-            horaini = "12:00:00"
-        elif  horaini == "12:30":
-            horaini = "12:30:00"
-        if horaini == "13:00":
-            horaini = "13:00:00"        
-        elif horaini == "13:30":
-            horaini = "13:30:00"        
-        elif  horaini == "14:00":
-            horaini = "14:00:00"
-        if horaini == "14:30":
-            horaini = "14:30:00"        
-        elif horaini == "15:00":
-            horaini = "15:00:00"
-        elif horaini == "15:30":
-            horaini = "15:30:00"        
-        elif  horaini == "16:00":
-            horaini = "16:00:00"
-        if horaini == "16:30":
-            horaini = "16:30:00"        
-        elif horaini == "17:00":
-            horaini = "17:00:00"        
-        elif  horaini == "17:30":
-            horaini = "17:30:00"
-        elif horaini == "18:00":
-            horaini = "18:00:00"        
-        elif  horaini == "18:30":
-            horaini = "18:30:00"
+        datosCita = CitaForm(request.POST, prefix="cita")
+        if (datosCita.is_valid()):
+            cita = datosCita.save(commit=False)   
+            cita.save()
+            print("Cita saved")
+            return redirect('citas_list')
+        print "datos is invalid"
+        print("Errors cita:", datosCita.errors)
+        return render(request,self.template_name,{'datosCitaForm':datosCita, 'pagina_actual': 'cita'})
 
 
-        horafin = request.POST.get('horafin', default = "")
+@method_decorator(login_required, name="dispatch")
+class CitasListView(ListView):
+    model = Cita
+    #paginate_by = 10
 
-        if horafin == "08:00":
-            horafin = "08:00:00"       
-        elif horafin == "08:30":
-            horafin = "08:30:00"        
-        elif  horafin == "09:00":
-            horafin = "09:00:00"
-        if horafin == "09:30":
-            horafin = "09:30:00"        
-        elif horafin == "10:00":
-            horafin = "10:00:00"        
-        elif  horafin == "10:30":
-            horafin = "10:30:00"
-        if horafin == "11:00":
-            horafin = "11:00:00"        
-        elif horafin == "11:30":
-            horafin = "11:30:00"        
-        elif  horafin == "12:00":
-            horafin = "12:00:00"
-        elif  horafin == "12:30":
-            horafin = "12:30:00"
-        if horafin == "13:00":
-            horafin = "13:00:00"        
-        elif horafin == "13:30":
-            horafin = "13:30:00"        
-        elif  horafin == "14:00":
-            horafin = "14:00:00"
-        if horafin == "14:30":
-            horafin = "14:30:00" 
-        elif horafin == "15:00":
-            horafin = "15:00:00"       
-        elif horafin == "15:30":
-            horafin = "15:30:00"        
-        elif  horafin == "16:00":
-            horafin = "16:00:00"
-        if horafin == "16:30":
-            horafin = "16:30:00"        
-        elif horafin == "17:00":
-            horafin = "17:00:00"        
-        elif  horafin == "17:30":
-            horafin = "17:30:00"
-        elif horafin == "18:00":
-            horafin = "18:00:00"        
-        elif  horafin == "18:30":
-            horafin = "18:30:00"
-        paciente_id = request.POST.get('paciente', default = "")
-        tipoterapia_id = request.POST.get('tipoterapia', default = "")
-        terapista_id = request.POST.get('terapista', default = "")
-        cita = Cita()
-        try:
-            cita.tipo_terapia = Tipo_terapia.objects.get(id=tipoterapia_id)
-        except Tipo_terapia.DoesNotExist:
-            print "error"
-        try:
-            cita.paciente = Paciente.objects.get(id=paciente_id)
-        except Paciente.DoesNotExist:
-            print "error"
-        try:
-            cita.terapista = Terapista.objects.get(id=terapista_id)
-        except Terapista.DoesNotExist:
-            print "error"
-        cita.fecha_cita=fechacita
-        cita.hora_inicio=horaini
-        cita.hora_fin=horafin
-        #cita = Cita(fecha_cita=fechacita,  hora_inicio=horaini, hora_fin=horafin, tipo_terapia=tipoterapia, paciente=paciente, terapista=terapista)
-        cita.save()
-        return render(request, self.template_name, {'pagina_actual': 'cita'})
-        return HttpResponseRedirect('/cita')
+    def get_context_data(self, **kwargs):
+        context = super(CitasListView, self).get_context_data(**kwargs)
+        context['pagina_actual'] = 'cita'
+        return context
+
+
+@method_decorator(login_required, name="dispatch")
+class CitaEditView(View):
+    template_name = 'cita/reservar_cita.html'
+   
+
+    def get(self, request, *args, **kwargs):
+        id_cita = kwargs.get('id_cita')
+        cita_object = get_object_or_404(Cita, pk=id_cita)    
+        datosCita = CitaForm(prefix="cita", instance=cita_object)   
+        return render(request, self.template_name,{'datosCitaForm':datosCita, 'pagina_actual': 'cita'})
+
+    def post(self, request, *args, **kwargs):
+        id_cita = kwargs.get('id_cita')
+        cita_object = get_object_or_404(Cita, pk=id_cita)
+        datosCita = CitaForm(request.POST, prefix="cita", instance=cita_object)
+        if (datosCita.is_valid()):
+            cita = datosCita.save()   
+            print("Cita saved")
+            return redirect('citas_list')
+        #print "datos is invalid"
+        print("Errors cita:", datosCita.errors)
+        return render(request,self.template_name,{'datosCitaForm':datosCita, 'pagina_actual': 'cita'})        
+
+
+@login_required
+def eliminar_cita_view(request):
+    respuesta = {}
+    if request.user.is_authenticated() and request.POST:
+        #grupos = request.user.groups.all()
+        user = request.user
+        cita = user.has_module_perms('cita')
+        print "\n\ncita: ", cita
+        if cita:
+            cita_id = request.POST.get("cita_id")
+            print cita_id, type(cita_id)
+            cita = Cita.objects.get(pk=cita_id)
+            deletion = cita.delete()
+            respuesta = {"mensaje": "cita eliminada", "result": deletion}
+
+        else:
+            respuesta = {"error": "No tiene permisos para eliminar esta cita"}
+        #registro = grupos.filter(name='registro').count() == 1
+    else:
+        respuesta = {"error": "no es posible eliminar la cita"}
+    data = json.dumps(respuesta)
+    return HttpResponse(data, content_type="application/json")
+
+
+@login_required
+def cita_view(request, id_cita=""):
+    """Vista para mostrar detalles de una cita"""
+    cita = get_object_or_404(Cita, pk=id_cita)
+    assert False
+    return render(request, "cita/cita_view.html",
+                  {'pagina_actual': 'cita_view',
+                   'cita': cita})
+    
+
+#@method_decorator(login_required, name="dispatch")
+class BusquedaCitasView(View):
+
+    def post(self, request, *args, **kwargs):
+      try:
+          busqueda = request.POST['busqueda']
+          print busqueda
+          #busacamos los parecidos
+          respuesta = Cita.objects.filter(Q(paciente__icontains=busqueda))
+          #data = serializers.serialize("json", pacientes)
+          #return HttpResponse(data, content_type="application/json")
+      except MultiValueDictKeyError:
+          respuesta = {"mensaje":"busqueda_error"}
+          print "error"
+      data = serializers.serialize("json", respuesta)
+      return HttpResponse(data, content_type="application/json")
