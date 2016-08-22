a5e2b3143bda37cc0009cb867c54a6706854666c
Raul Rodriguez <rsrodrig@espol.edu.ec>
2016-08-21 22:53:07 -0500

    reservar cita jweekcalendar

--- cita/views.py
+++ cita/views.py
@@ -1,173 +1,121 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render, render_to_response, redirect, get_object_or_404
-from django.template import RequestContext
-from django.http import HttpResponseRedirect
-from django.views.generic import View, ListView
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.forms import formset_factory
-from django.db.models import Q
-from django.core import serializers
-from django.http.response import HttpResponse
-import json
-from cita.forms import *
-from modelos.cita_model  import Cita
-from asistencia.modelos.Terapista_model  import Terapista
-from asistencia.modelos.Tipo_terapia_model  import Tipo_terapia
-from asistencia.modelos.Terapia__Tipo_terapia_models  import Terapia__Tipo_terapia
-from registro.modelos.paciente_model  import Paciente
+from django.forms                   import formset_factory
+from django.http                    import HttpResponseRedirect
+from django.shortcuts               import render, render_to_response
+from django.template                import RequestContext
+from django.utils.decorators        import method_decorator
+from django.views.generic           import View
+#from .forms                         import *
+#from forms                      import CitaForm
+from asistencia.modelos.Terapista_model                 import Terapista
+from asistencia.modelos.Tipo_terapia_model              import Tipo_terapia
+from asistencia.modelos.Terapia__Tipo_terapia_models    import Terapia__Tipo_terapia
+from registro.modelos.paciente_model                    import Paciente
 
 
 @method_decorator(login_required, name="dispatch")
 class ReservarCitaView(View):
     template_name = 'cita/reservar_cita.html'
-    def get(self, request, *args, **kwargs):
-        datosCita = CitaForm(prefix="cita")
-        return render(request,self.template_name,{'datosCitaForm':datosCita, 'pagina_actual': 'cita'})
 
-    def post(self, request, *args, **kwargs):
-        datosCita = CitaForm(request.POST, prefix="cita")
-        if (datosCita.is_valid()):
-            cita = datosCita.save(commit=False)   
-            cita.save()
-            print("Cita saved")
-            return redirect('citas_list')
-        print "datos is invalid"
-        print("Errors cita:", datosCita.errors)
-        return render(request,self.template_name,{'datosCitaForm':datosCita, 'pagina_actual': 'cita'})
-
-
-@method_decorator(login_required, name="dispatch")
-class CitasListView(ListView):
-    model = Cita
-    #paginate_by = 10
-
-    def get_context_data(self, **kwargs):
-        context = super(CitasListView, self).get_context_data(**kwargs)
-        context['pagina_actual'] = 'cita'
-        return context
-
-
-@method_decorator(login_required, name="dispatch")
-class CitaEditView(View):
     template_name = 'cita/reservar_cita.html'
-   
-
     def get(self, request, *args, **kwargs):
-        id_cita = kwargs.get('id_cita')
-        cita_object = get_object_or_404(Cita, pk=id_cita)    
-        datosCita = CitaForm(prefix="cita", instance=cita_object)   
-        return render(request, self.template_name,{'datosCitaForm':datosCita, 'pagina_actual': 'cita'})
+        pacientes = Paciente.objects.order_by('fecha_registro')
+        tipo_terapia_list = Tipo_terapia.objects.order_by('costo')
+        terapista_list = Terapista.objects.order_by('fecha_nacimiento')
+        return render(request,self.template_name,{'pacientes':pacientes, 'tipo_terapia_list':tipo_terapia_list, 'terapista_list':terapista_list, 'pagina_actual': 'cita'})
 
     def post(self, request, *args, **kwargs):
-        id_cita = kwargs.get('id_cita')
-        cita_object = get_object_or_404(Cita, pk=id_cita)
-        datosCita = CitaForm(request.POST, prefix="cita", instance=cita_object)
-        if (datosCita.is_valid()):
-            cita = datosCita.save()   
-            print("Cita saved")
-            return redirect('citas_list')
-        #print "datos is invalid"
-        print("Errors cita:", datosCita.errors)
-        return render(request,self.template_name,{'datosCitaForm':datosCita, 'pagina_actual': 'cita'})        
+        fechacita = request.POST.get('date_holder', default = "")
+        annio = fechacita[8:]
+        mes   = fechacita[0:3]
+        dia   = fechacita[4:6]
+        if   mes == "Ene":
+            mes = "01"
+        elif mes == "Feb":
+            mes = "02"
+        elif mes == "Mar":
+            mes = "03"
+        elif mes == "Abr":
+            mes = "04"
+        elif mes == "May":
+            mes = "05"
+        elif mes == "Jun":
+            mes = "06"
+        elif mes == "Jul":
+            mes = "07"
+        elif mes == "Ago":
+            mes = "08"
+        elif mes == "Sep":
+            mes = "09"
+        elif mes == "Oct":
+            mes = "10"
+        elif mes == "Nov":
+            mes = "11"
+        elif mes == "Dic":
+            mes = "12"
+        fechacita = annio + '-' + mes + '-' + dia
+        horaini = request.POST.get('start', default = "")
+
+        if horaini == "08:00 am":
+            horaini = "08:00"
+        elif horaini == "08:15 am":
+            horaini = "08:15"
+        elif horaini == "08:30 am":
+            horaini = "08:30"
+        elif horafin == "08:45 am":
+            horafin = "08:45"
+        elif  horaini == "09:00 am":
+            horaini = "09:00"
+
+        horafin = request.POST.get('end', default = "")
+
+        if horafin == "08:00 am":
+            horafin = "08:00"
+        elif horafin == "08:15 am":
+            horafin = "08:15"
+        elif horafin == "08:30 am":
+            horafin = "08:30"
+        elif horafin == "08:45 am":
+            horafin = "08:45"
+        elif  horafin == "09:00 am":
+            horafin = "09:00"
+
+        paciente_id = request.POST.get('paciente', default = "")
+        tipoterapia_id = request.POST.get('tipoterapia', default = "")
+        terapista_id = request.POST.get('terapista', default = "")
+        cita = Cita()
+        try:
+            cita.tipo_terapia = Tipo_terapia.objects.get(id=tipoterapia_id)
+        except Tipo_terapia.DoesNotExist:
+            print "error"
+        try:
+            cita.paciente = Paciente.objects.get(id=paciente_id)
+        except Paciente.DoesNotExist:
+            print "error"
+        try:
+            cita.terapista = Terapista.objects.get(id=terapista_id)
+        except Terapista.DoesNotExist:
+            print "error"
+        #cita.fecha_cita=fechacita
+        cita.hora_inicio=horaini
+        cita.hora_fin=horafin
+        #cita = Cita(fecha_cita=fechacita,  hora_inicio=horaini, hora_fin=horafin, tipo_terapia=tipoterapia, paciente=paciente, terapista=terapista)
+        cita.save()
+        return render(request, self.template_name, {'pagina_actual': 'cita'})
+        return HttpResponseRedirect('/cita')
 
 
-@login_required
-def eliminar_cita_view(request):
-    respuesta = {}
-    if request.user.is_authenticated() and request.POST:
-        #grupos = request.user.groups.all()
-        user = request.user
-        cita = user.has_module_perms('cita')
-        print "\n\ncita: ", cita
-        if cita:
-            cita_id = request.POST.get("cita_id")
-            print cita_id, type(cita_id)
-            cita = Cita.objects.get(pk=cita_id)
-            deletion = cita.delete()
-            respuesta = {"mensaje": "cita eliminada", "result": deletion}
-
-        else:
-            respuesta = {"error": "No tiene permisos para eliminar esta cita"}
-        #registro = grupos.filter(name='registro').count() == 1
-    else:
-        respuesta = {"error": "no es posible eliminar la cita"}
-    data = json.dumps(respuesta)
-    return HttpResponse(data, content_type="application/json")
 
 
-@login_required
-def cita_view(request, id_cita=""):
-    """Vista para mostrar detalles de una cita"""
-    cita = get_object_or_404(Cita, pk=id_cita)
-    assert False
-    return render(request, "cita/cita_view.html",
-                  {'pagina_actual': 'cita_view',
-                   'cita': cita})
-    
 
-#@method_decorator(login_required, name="dispatch")
-class BusquedaCitasView(View):
-    def post(self, request, *args, **kwargs):
-      try:
-          busqueda = request.POST['busqueda']
-          print busqueda
-          #busacamos los parecidos
-          respuesta = Cita.objects.filter(Q(fecha_cita__icontains=busqueda))
-          #data = serializers.serialize("json", pacientes)
-          #return HttpResponse(data, content_type="application/json")
-      except MultiValueDictKeyError:
-          respuesta = {"mensaje":"busqueda_error"}
-          print "error"
-      data = serializers.serialize("json", respuesta)
-      return HttpResponse(data, content_type="application/json")
-
-#@method_decorator(login_required, name="dispatch")
-class BusquedaTerapistaView(View):
-    def post(self, request, *args, **kwargs):
-      try:
-          terapista_id = request.POST['terapista_id']
-          #id_terapista = kwargs.get('terapista_id')
-          print terapista_id
-          #buscamos los parecidos           
-          respuesta = Terapista.objects.filter(Q(id__icontains=terapista_id))          
-          #data = serializers.serialize("json", pacientes)
-          #return HttpResponse(data, content_type="application/json")
-      except MultiValueDictKeyError:
-          respuesta = {"mensaje":"busqueda_error"}
-          print "error"
-      data = serializers.serialize("json", respuesta)
-      return HttpResponse(data, content_type="application/json")
-
-#@method_decorator(login_required, name="dispatch")
-class BusquedaTipoTerapiaView(View):
-    def post(self, request, *args, **kwargs):
-      try:
-          tipo_terapia_id = request.POST['tipo_terapia_id']
-          #id_tipoterapia = kwargs.get('tipo_terapia_id')
-          print tipo_terapia_id
-          #buscamos los parecidos           
-          respuesta = Tipo_terapia.objects.filter(Q(id__icontains=tipo_terapia_id))          
-          #data = serializers.serialize("json", pacientes)
-          #return HttpResponse(data, content_type="application/json")
-      except MultiValueDictKeyError:
-          respuesta = {"mensaje":"busqueda_error"}
-          print "error"
-      data = serializers.serialize("json", respuesta)
-      return HttpResponse(data, content_type="application/json")
-
-#@method_decorator(login_required, name="dispatch")
-class BusquedaPacienteView(View):
-    def post(self, request, *args, **kwargs):
-      try:
-          paciente_id = request.POST['paciente_id']
-          print paciente_id
-          #buscamos los parecidos           
-          respuesta = Paciente.objects.filter(Q(id__icontains=paciente_id))          
-          #data = serializers.serialize("json", pacientes)
-          #return HttpResponse(data, content_type="application/json")
-      except MultiValueDictKeyError:
-          respuesta = {"mensaje":"busqueda_error"}
-          print "error"
-      data = serializers.serialize("json", respuesta)
-      return HttpResponse(data, content_type="application/json")
+        # datos_cita = CitaForm(request.POST, prefix="cita")
+        # if datos_cita.is_valid():
+        #     #cita = datos_cita.save(commit=False) 
+        #     cita.save() 
+        #     print "datos de la cita grabados exitosamente"          
+        #     context = {'cita_form': datos_cita,
+        # 		       'pagina_actual': 'cita'}
+        # #return render(request, self.template_name, context)
+        # return render_to_response(self.template_name, context, context_instance=RequestContext(request))
+        # #return render_to_response(self.template_name, context, RequestContext(request))
+        # return HttpResponseRedirect('/cita')
