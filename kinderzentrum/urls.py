7248a82fe9044650a23d205d691200fa853dbfa5
Raul Rodriguez <rsrodrig@espol.edu.ec>
2016-09-19 03:14:11 -0500

    cita.views.get_citas

--- kinderzentrum/urls.py
+++ kinderzentrum/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 import home
+import cita
 
 urlpatterns = [
     # Examples:
@@ -20,5 +21,11 @@ urlpatterns = [
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^change/password/$', home.views.change_password, name='changepassword'),
+
+    url(r'^get_citas/$', cita.views.get_citas, name='get_citas'),
+    url(r'^get_cita/$', cita.views.get_cita, name='get_cita'),
+    url(r'^save_cita/$', cita.views.save_cita, name='save_cita'),
+    url(r'^delete_cita/$', cita.views.delete_cita, name='delete_cita'),
+    url(r'^update_cita/$', cita.views.update_cita, name='update_cita'),
 
 ]
