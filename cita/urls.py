793b66fbb11d4087181c2b02b403324d52742d64
Raul Rodriguez <rsrodrig@espol.edu.ec>
2016-09-21 22:02:52 -0500

    cita/urls.py

--- cita/urls.py
+++ cita/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 from cita.views import ReservarCitaView
+from .views import *
 
 urlpatterns =[ 
     
     url(r'^$',  ReservarCitaView.as_view(), name='reservar_cita_view'),
+    
     
 ]
